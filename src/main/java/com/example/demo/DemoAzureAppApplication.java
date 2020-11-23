a2df43049f8d34f0d91542a057871f63d730ee89
purin-it <stanahas@amber.plala.or.jp>
2020-11-23 21:01:05 +0900

    ServletInitializer merge to DemoAzureAppApplication

--- src/main/java/com/example/demo/DemoAzureAppApplication.java
+++ src/main/java/com/example/demo/DemoAzureAppApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoAzureAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoAzureAppApplication.class, args);
+public class DemoAzureAppApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoAzureAppApplication.class);
 	}
+	
+	public static void main(String[] args) {
+        SpringApplication.run(DemoAzureAppApplication.class, args);
+    }
 
 }
