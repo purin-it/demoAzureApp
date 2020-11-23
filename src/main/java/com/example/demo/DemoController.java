862ae934469078303851f3df97815366baaeac06
purin-it <stanahas@amber.plala.or.jp>
2020-11-23 20:31:30 +0900

    DemoController Updated.

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
 
-	@GetMapping("/")
+	//@GetMapping("/")
+	@RequestMapping("/")
 	public String index(Model model){
 		model.addAttribute("message", "Hello App Service!!");
 		return "index";
